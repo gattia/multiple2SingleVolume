e595783c339e338168a42a75db7145470e016c40
Gatti <anthony.a.gatti@gmail.com>
2016-02-12 11:22:14 -0500

    program works & returns singleVolume image

--- multi2SingleVolume/multi2SingleVolume.py
+++ multi2SingleVolume/multi2SingleVolume.py
@@ -110,7 +110,56 @@ class multi2SingleVolumeWidget(ScriptedLoadableModuleWidget):
      inputVolume = self.inputSelector.currentNode()
      outputVolume = self.outputSelector.currentNode()
      
+     mvNode = slicer.util.getNode(inputVolume.GetID())
+     mvData = mvNode.GetImageData()
      
+     extract = vtk.vtkImageExtractComponents()
+     extract.SetInputData(mvData)
+     extract.SetComponents(0)
+     extract.Update()
+     
+     scalarVolumeNode = slicer.vtkMRMLScalarVolumeNode()
+     scalarVolumeNode.SetScene(slicer.mrmlScene)
+     slicer.mrmlScene.AddNode(scalarVolumeNode)
+     
+     ras2ijk = vtk.vtkMatrix4x4()
+     ijk2ras = vtk.vtkMatrix4x4()
+     
+     mvNode.GetRASToIJKMatrix(ras2ijk)
+     mvNode.GetIJKToRASMatrix(ijk2ras)
+     scalarVolumeNode.SetRASToIJKMatrix(ras2ijk)
+     scalarVolumeNode.SetIJKToRASMatrix(ijk2ras)
+     
+     scalarVolumeNode.SetAndObserveImageData(extract.GetOutput())
+     displayNode = scalarVolumeNode.GetDisplayNode()
+     
+     scalarVolumeNode.SetName('singleVolume')
+
+# mvNode = slicer.util.getNode('T2MAP*')
+# mvData = mvNode.GetImageData()
+
+# extract = vtk.vtkImageExtractComponents()
+# extract.SetInputData(mvData)
+# extract.SetComponents(0)
+# extract.Update()
+#
+# scalarVolumeNode = slicer.vtkMRMLScalarVolumeNode()
+# scalarVolumeNode.SetScene(slicer.mrmlScene)
+# slicer.mrmlScene.AddNode(scalarVolumeNode)
+#
+# ras2ijk = vtk.vtkMatrix4x4()
+# ijk2ras = vtk.vtkMatrix4x4()
+#
+# mvNode.GetRASToIJKMatrix(ras2ijk)
+# mvNode.GetIJKToRASMatrix(ijk2ras)
+# scalarVolumeNode.SetRASToIJKMatrix(ras2ijk)
+# scalarVolumeNode.SetIJKToRASMatrix(ijk2ras)
+#
+# scalarVolumeNode.SetAndObserveImageData(extract.GetOutput())
+# displayNode = scalarVolumeNode.GetDisplayNode()
+#
+# scalarVolumeNode.SetName('singleVolume')
+
 
 
 ##
